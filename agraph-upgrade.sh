baba40319881a9302adfe5c1c9931faf326726d3
rodentrabies <rodentrabies@protonmail.com>
2025-02-27 01:12:13 +0200

    tools: correctly handle non-test AG builds in agraph-upgrade.sh

--- agraph-upgrade.sh
+++ agraph-upgrade.sh
@@ -5,8 +5,9 @@
 
 source agraph-config.sh
 
+AGRAPH_VERSION_NO_TAG=$(echo "$AGRAPH_VERSION" | sed -n 's/.*\b\([0-9]\+\.[0-9]\+\.[0-9]\+\)\b.*/\1/p')
 BASE_URL="https://franz.com/ftp/pri/acl/ag"
-DIST_NAME="agraph-${AGRAPH_VERSION%.*}-linuxamd64.64smp-direct-client-lisp-acl10.1"
+DIST_NAME="agraph-${AGRAPH_VERSION_NO_TAG}-linuxamd64.64smp-direct-client-lisp-acl10.1"
 ARCHIVE_NAME="${DIST_NAME}.tar.gz"
 FULL_URL="${BASE_URL}/ag${AGRAPH_VERSION}/linuxamd64.64smp/${ARCHIVE_NAME}"
 
